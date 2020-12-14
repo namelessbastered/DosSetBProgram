60deaffe1d58f5fe2cb2fb4037fc7d0eb9243080
namelessbastered <75994966+namelessbastered@users.noreply.github.com>
2020-12-14 22:44:44 +0800

    Mao lang (2)

--- Dos_Implementations.cpp
+++ Dos_Implementations.cpp
@@ -8,18 +8,15 @@
 struct Node* newNode(char* data)
 {
     struct Node* node = (struct Node*)malloc(sizeof(struct Node));
-    //node->data = data;
     strcpy_s(node->data, data);
     node->left = NULL;
     node->right = NULL;
     return node;
 }
 
-
-
 void print2DUtil(Node* root, int space)
 {
- 
+
     // Base case 
     if (root == NULL)
         return;
@@ -34,35 +31,54 @@ void print2DUtil(Node* root, int space)
     printf("\n");
     for (int i = COUNT; i < space; i++)
         printf("  ");
-    
+
     printf("%s<", root->data);
-    
+
     // Process left child 
     print2DUtil(root->left, space);
 
 }
 
-// Wrapper over print2DUtil() 
 void print2D(Node* root)
 {
     // Pass initial space count as 0 
     print2DUtil(root, 0);
 }
 
-void printList(Node* root){
+void printList(Node* root) {
 }
-
-
 
 Node* search(Node* root, char* data)
 {
 
     if (root == NULL)
         return NULL;
-    else if ((strcmp(root->data, data)==0))
+    else if ((strcmp(root->data, data) == 0))
         return root;
     else {
         Node* left = search(root->left, data);
         return left ? left : search(root->right, data);
     }
+}
+
+void downline(Node* root, char* tmpUpID, char* tmpID) {
+
+    int insDone = 0;
+
+    Node* tmpHolder = search(root, tmpUpID);
+    printf("\nNode found in: %p", tmpHolder);
+    if (tmpHolder == NULL) {
+        printf("\nERROR - UPLINE NOT FOUND!");
+        insDone = 1;
+    }
+    if (insDone != 1 && tmpHolder->left == NULL) {
+        tmpHolder->left = newNode(tmpID);
+        insDone = 1;
+    }
+    if (insDone != 1 && tmpHolder->right == NULL) {
+        tmpHolder->right = newNode(tmpID);
+        insDone = 1;
+    }
+
+    insDone = 0;
 }
